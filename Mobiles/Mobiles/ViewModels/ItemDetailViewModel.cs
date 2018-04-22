b2497e5e8709b73b5b1036d68c2d91bc37f32b95
ocph23 <ocph23@hotmail.com>
2018-04-22 21:33:23 +0900

    Add project files.

---
+++ Mobiles/Mobiles/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mobiles.Models;
+
+namespace Mobiles.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
